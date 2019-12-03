d6b8e99cdb0ac7ba829b9ba322ca9d57ac7deb88
jgonz100 <52713480+jgonz100@users.noreply.github.com>
2019-12-03 12:22:24 -0800

    Create multDisp.jl

---
+++ multDisp.jl
@@ -0,0 +1,8 @@
+# In this function we specifically say that the arguments can only be of type int
+addArgs(x::int, y::int) = x+y
+addArgs(1,1) # 2
+addArgs(1,1.1) # Error
+
+multArgs(x::Number, y::Number) = 3x - 2y
+multArgs(4,2)
+# The function will run as long as the arguments are instances of Number which includes all real numbers
